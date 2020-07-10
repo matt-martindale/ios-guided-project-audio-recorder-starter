bd5b13512ae5ad29e24b8a1823153b7d9d3ecec7
Matt Martindale <martindalematt18@yahoo.com>
2020-07-09 21:18:49 -0600

    added recording functionality to the app

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -20,6 +20,8 @@ class AudioRecorderController: UIViewController {
     }
     
     weak var timer: Timer?
+    var recordingURL: URL?
+    var audioRecorder: AVAudioRecorder?
     
     @IBOutlet var playButton: UIButton!
     @IBOutlet var recordButton: UIButton!
@@ -150,18 +152,21 @@ class AudioRecorderController: UIViewController {
     
     // MARK: - Recording
     
+    var isRecording: Bool {
+        audioRecorder?.isRecording ?? false
+    }
+    
     func createNewRecordingURL() -> URL {
         let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         
         let name = ISO8601DateFormatter.string(from: Date(), timeZone: .current, formatOptions: .withInternetDateTime)
         let file = documents.appendingPathComponent(name, isDirectory: false).appendingPathExtension("caf")
         
-//        print("recording URL: \(file)")
+        print("recording URL: \(file)")
         
         return file
     }
     
-    /*
     func requestPermissionOrStartRecording() {
         switch AVAudioSession.sharedInstance().recordPermission {
         case .undetermined:
@@ -192,14 +197,29 @@ class AudioRecorderController: UIViewController {
             break
         }
     }
-    */
     
     func startRecording() {
+        do {
+            try prepareAudioSession()
+        } catch {
+            print("Cannot record audio: \(error)")
+            return
+        }
         
+        recordingURL = createNewRecordingURL()
+        
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44_100, channels: 1)!
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: recordingURL!, format: format)
+            audioRecorder?.record()
+        } catch {
+            preconditionFailure("The audio recorder could not be created with \(recordingURL!) and \(format): \(error)")
+        }
     }
     
     func stopRecording() {
-        
+        audioRecorder?.stop()
     }
     
     // MARK: - Actions
@@ -222,7 +242,11 @@ class AudioRecorderController: UIViewController {
     }
     
     @IBAction func toggleRecording(_ sender: Any) {
-        
+        if isRecording {
+            stopRecording()
+        } else {
+            requestPermissionOrStartRecording()
+        }
     }
     
 }
