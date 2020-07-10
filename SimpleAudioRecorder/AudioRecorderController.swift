8902694d429248ce7f32e0e8cc7f909231add32d
Matt Martindale <martindalematt18@yahoo.com>
2020-07-09 20:57:48 -0600

    added an audio waveForm during audio playback

--- SimpleAudioRecorder/AudioRecorderController.swift
+++ SimpleAudioRecorder/AudioRecorderController.swift
@@ -15,6 +15,7 @@ class AudioRecorderController: UIViewController {
         didSet {
             guard let audioPlayer = audioPlayer else { return }
             audioPlayer.delegate = self
+            audioPlayer.isMeteringEnabled = true
         }
     }
     
@@ -92,12 +93,12 @@ class AudioRecorderController: UIViewController {
 //
 //            }
 //
-//            if let audioPlayer = self.audioPlayer,
-//                self.isPlaying == true {
-//
-//                audioPlayer.updateMeters()
-//                self.audioVisualizer.addValue(decibelValue: audioPlayer.averagePower(forChannel: 0))
-//            }
+            if let audioPlayer = self.audioPlayer,
+                self.isPlaying == true {
+
+                audioPlayer.updateMeters()
+                self.audioVisualizer.addValue(decibelValue: audioPlayer.averagePower(forChannel: 0))
+            }
         }
     }
     
